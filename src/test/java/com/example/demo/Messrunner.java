d023b9968821897284882963aa30b1ac698e30ff
Druthi <Druthi@192.168.1.4>
2020-07-30 11:18:47 +1200

    message

---
+++ src/test/java/com/example/demo/Messrunner.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class Messrunner {
+	 public static void main(String args[]) 
+	    { 
+	        System.out.println("Hello, World"); 
+	    } 
+
+}
---
+++ src/test/java/com/example/demo/SpringBoottestAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoottestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("hello ram");
+	}
+
+}
